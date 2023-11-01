69926a23881c4dbe69988ba997ac7f0488fd7cb2
Joshua12450 <Joshua12450@gmail.com>
2023-11-01 00:59:50 -0400

    added firing sounds and animations

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+    public float fireRate;
+    protected float lastFireTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFireTime = Time.time - 10;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+    }
+    protected void Fire()
+    {
+        if (ammo.HasAmmo(tag))
+        {
+        GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
+        GetComponentInChildren<Animator>().Play("Fire");
+    }
+}
